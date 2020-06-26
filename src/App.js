c50d05c776a776da4bf2a2c38f3648b75afc5a7c
Ryan Sandzimier <rsandz@mit.edu>
2020-06-26 16:08:50 -0700

    Fixed bug in gameServer and lobbyServer heroku hyperlinks

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ import { SocketIO } from 'boardgame.io/multiplayer';
 import { BattleLine } from './Game';
 import { BattleLineBoard } from './Board';
 
-const BattleLineClient = Client({
-  game: BattleLine,
-  board: BattleLineBoard,
-  multiplayer: SocketIO({ server: 'https://battleline-backend.herokuapp.com/' }),
-});
-console.log(window);
-console.log(window.location);
-console.log(window.location.hostname);
 const App = () => (
   <div>
-    <BattleLineClient/>
     <Lobby
-    gameServer={'https://battleline-backend.herokuapp.com/'}
-    lobbyServer={'https://battleline-backend.herokuapp.com/'}
-    // gameServer={'localhost:8000'}
-    // lobbyServer={'localhost:8000'}
+    gameServer={'https://battleline-backend.herokuapp.com'}
+    lobbyServer={'https://battleline-backend.herokuapp.com'}
     gameComponents={[{game: BattleLine, board: BattleLineBoard}]}
     />
   </div>
