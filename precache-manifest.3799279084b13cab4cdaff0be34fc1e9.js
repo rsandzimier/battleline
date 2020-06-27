be61c0659b207d38eac72f1c4f0f4da64bce073b
Ryan Sandzimier <rsandz@mit.edu>
2020-06-27 03:49:20 -0700

    Updates

---
+++ precache-manifest.3799279084b13cab4cdaff0be34fc1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2a58014dba88d25f89938b61ed64cb",
+    "url": "/battleline/index.html"
+  },
+  {
+    "revision": "063a6b6c20480ea9378d",
+    "url": "/battleline/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2518120b48146d7b700c",
+    "url": "/battleline/static/js/2.8b44eb7e.chunk.js"
+  },
+  {
+    "revision": "581070db7478fba8a52f7e9e4c335e11",
+    "url": "/battleline/static/js/2.8b44eb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063a6b6c20480ea9378d",
+    "url": "/battleline/static/js/main.c05919d0.chunk.js"
+  },
+  {
+    "revision": "fa33b2e79b33f9d26a51",
+    "url": "/battleline/static/js/runtime-main.0a2e9051.js"
+  }
+]);
