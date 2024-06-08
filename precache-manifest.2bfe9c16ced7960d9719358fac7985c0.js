744c45d946f6266de1648e7ade2494c4f984a10e
ryan <rsandzimier@gmail.com>
2024-06-08 10:42:58 -0700

    Updates

---
+++ precache-manifest.2bfe9c16ced7960d9719358fac7985c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ae54d39895ed5f542021539e41cf7b",
+    "url": "/battleline/index.html"
+  },
+  {
+    "revision": "9605c30336010cb3f378",
+    "url": "/battleline/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52d94408cf9650845eb8",
+    "url": "/battleline/static/js/2.16b6f4fc.chunk.js"
+  },
+  {
+    "revision": "4410cd93440cab7c3419c8c0d0bfccdd",
+    "url": "/battleline/static/js/2.16b6f4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9605c30336010cb3f378",
+    "url": "/battleline/static/js/main.31b536bd.chunk.js"
+  },
+  {
+    "revision": "c0780c5da2f4bd425bde",
+    "url": "/battleline/static/js/runtime-main.01dfb113.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/battleline/precache-manifest.ad88d58b673a7fcc431096b575e84995.js"
+  "/battleline/precache-manifest.2bfe9c16ced7960d9719358fac7985c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/battleline/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
